022990a9dc3971f18c7c286a8e72e4468d08366f
poncenicolas086-hash <poncenicolas086@gmail.com>
2026-07-22 19:14:30 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -563,7 +563,7 @@ export default function App() {
     setMatchScript(scriptMap);
   };
 
-  const startPenaltyShootoutSequence = (currentEvents: any[]) => {
+  const startPenaltyShootoutSequence = (currentEvents: any[], currentScorers: any[]) => {
     setInPenaltyShootout(true);
     setPenaltyStep(0);
     setPenaltyKicksLog([]);
